a2c847a2a7aa26b09c597a53ff01298ec2115f53
Vivin.M <vivinmani20@gmail.com>
2023-12-13 15:14:12 +0530

    Day2

---
+++ Day2/sampleapp/src/App.js
@@ -0,0 +1,22 @@
+import './assests/css/style.css'
+function App() {
+  return (
+    <div>
+     
+    <div class="navbar" >
+        <div class="navtitle">
+           NAVBAR
+        </div>
+    
+    <ul class="navlinks">
+       <li> <button type="submit"><a href="login.html"> Login</a></button></li>
+        <li><button  type="submit" ><a href="sign up.html"> Sign Up</a></button></li>
+    </ul>
+    </div>
+
+      </div>
+  
+  )
+  }
+
+export default App
