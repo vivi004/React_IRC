59b6d11c52953fdfdb15084f8f6b8006ac7e79f1
Vivin.M <vivinmani20@gmail.com>
2023-12-18 12:34:20 +0530

    Day6

---
+++ Day6/myapp/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from './Components/Nav'
+import Home from './Components/Home';
+function App() {
+  return (
+    <>
+    <Nav/>
+    <Home/>
+    </>
+  );
+}
+
+export default App;
